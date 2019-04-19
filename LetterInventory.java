1c29502bf5dd6403ca1cf704956c6582bf458186
jnsmelinda <jns.melinda@gmail.com>
2019-04-18 19:18:34 -0700

    create LetterInventory
    test1 and test2 passed

---
+++ LetterInventory.java
@@ -0,0 +1,64 @@
+public class LetterInventory {
+
+    private final int[] COUNTER = new int[26];
+    private int size;
+
+    public LetterInventory(String word) {
+        word = word.toLowerCase();
+        for (int i = 0; i < word.length(); i++) {
+            if ((word.charAt(i) >= 'a' && word.charAt(i) <= 'z')){
+                COUNTER[getIndex(word.charAt(i))]++;
+                size++;
+            }
+        }
+    }
+
+    public int get(char letter){
+        return COUNTER[getIndex(letter)];
+    }
+
+    public void set(char letter, int value) throws IllegalArgumentException{
+        if (value < 0) {
+            throw new IllegalArgumentException("value < 0");
+        }
+        if (getIndex(letter) > 26 || getIndex(letter) < 0){
+            throw new IllegalArgumentException("non-alphabetic character");
+        }
+
+        getNewSize(letter, value);
+        COUNTER[getIndex(letter)] = value;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        for (int i = 0; i < COUNTER.length; i++) {
+            if (COUNTER[i] > 0) {
+                for (int j = 0; j < COUNTER[i]; j++){
+                    sb.append((char) ((char)i + 'a'));
+                }
+            }
+        }
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+    private int getIndex(char letter){
+        return Character.getNumericValue(letter) - 10;
+    }
+
+    private void getNewSize(char letter, int value) {
+        size = (size - get(letter)) + value;
+    }
+
+}
