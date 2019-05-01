112d866e859fc1817b383c2b83d68d32bc31d760
jnsmelinda <jns.melinda@gmail.com>
2019-05-01 12:13:44 -0700

    works
    opportunities: current.next to current and current to prev in the killTheRest() method

--- AssassinManager/AssassinManager.java
+++ AssassinManager/AssassinManager.java
@@ -18,11 +18,12 @@ public class AssassinManager {
         headOfRing = current;
     }
 
-    public boolean gameOver() {
-        return headOfRing.next == null;
+    public boolean graveyardContains(String name) {
+        return contains(name, headOfGrave);
     }
 
-    public void printGraveyard() {
+    public boolean killRingContains(String name) {
+        return contains(name, headOfRing);
     }
 
     public String winner() {
@@ -34,38 +35,141 @@ public class AssassinManager {
         }
     }
 
-    public void printKillRing() {
-        AssassinNode current = headOfRing;
-        AssassinNode last = null;
-        while (current.next != null){
-            System.out.println("    " + current.name + " is stalking " + current.next.name);
-            current = current.next;
-            if (current.next == null){
-                last = current;
+    public boolean gameOver() {
+        return headOfRing.next == null;
+    }
+
+    public void printGraveyard() {
+        AssassinNode current = headOfGrave;
+        if (headOfGrave != null && headOfGrave.next == null) {
+            print(current.name, "was killed by", current.killer);
+        }
+        else {
+            current = reverse(current);
+            while (current != null) {
+                print(current.name, "was killed by", current.killer);
+                current = current.next;
             }
         }
-        System.out.println("    " + last.name + " is stalking " + headOfRing.name);
-
     }
 
-    public boolean graveyardContains(String name) {
-        return contains(name, headOfGrave);
-    }
-
-    public boolean killRingContains(String name) {
-        return contains(name, headOfRing);
+    public void printKillRing() {
+        AssassinNode current = headOfRing;
+        while (current.next != null){
+            print(current.name, "is stalking", current.next.name);
+            current = current.next;
+        }
+        print(current.name, "is stalking", headOfRing.name);
     }
 
     public void kill(String name) {
+        if (!killRingContains(name)) {
+            throw new IllegalArgumentException("not in the ring");
+        }
+        if (gameOver()) {
+            throw new IllegalStateException("the game is over");
+        }
+        if (headOfRing.name.equalsIgnoreCase(name)) {
+            killTheFirst();
+        }
+        else {
+            killTheRest(name);
+        }
     }
 
-    private boolean contains(String name, AssassinNode current){
-        while (current.next != null) {
-            if (current.name.equalsIgnoreCase(name) || current.next.name.equalsIgnoreCase(name)){
-                return true;
+    private void killTheRest(String name) {
+        AssassinNode victim;
+        AssassinNode current = headOfRing;
+        while (current.next != null){
+            if (current.next.name.equalsIgnoreCase(name)){
+                victim = current.next;
+                victim.killer = current.name;
+                if (isEmpty(headOfGrave)) {
+                    headOfGrave = victim;
+                    if (current.next.next == null){
+                        current.next = null;
+                        break;
+                    }
+                    current.next = current.next.next;
+                    headOfGrave.next = null;
+                }
+                else {
+                    AssassinNode dead = headOfGrave;
+                    while (dead.next != null){
+                        dead = dead.next;
+                    }
+                    dead.next = victim;
+                    if (current.next.next == null){
+                        current.next = null;
+                        break;
+                    }
+                    current.next = current.next.next;
+                    dead.next.next = null;
+                }
             }
             current = current.next;
         }
+    }
+
+    private void killTheFirst(){
+        AssassinNode victim;
+        victim = headOfRing;
+        if (isEmpty(headOfGrave)) {
+            headOfGrave = victim;
+            victim.killer = getLast(headOfRing).name;
+            headOfRing = headOfRing.next;
+            headOfGrave.next = null;
+        } else {
+            AssassinNode current = headOfGrave;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = victim;
+            victim.killer = getLast(headOfRing).name;
+            headOfRing = headOfRing.next;
+            current.next.next = null;
+        }
+    }
+
+    private void print(String name1, String text, String name2){
+        System.out.println("    " + name1 + " " + text + " " + name2);
+    }
+
+    private AssassinNode reverse(AssassinNode current) {
+        AssassinNode prev = null;
+        AssassinNode next;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        current = prev;
+        return current;
+    }
+
+    private AssassinNode getLast(AssassinNode current){
+        AssassinNode last = null;
+        while (current.next != null){
+            last = current.next;
+            current = current.next;
+        }
+        return last;
+    }
+
+    private boolean contains(String name, AssassinNode current){
+        if (current != null) {
+            while (current.next != null) {
+                if (current.name.equalsIgnoreCase(name) || current.next.name.equalsIgnoreCase(name)) {
+                    return true;
+                }
+                current = current.next;
+            }
+        }
         return false;
+    }
+
+    private boolean isEmpty(AssassinNode current) {
+        return (current == null);
     }
 }
