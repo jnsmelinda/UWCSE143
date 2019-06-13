8b7228645b675693b75288957127fc701ff9f830
jnsmelinda <jns.melinda@gmail.com>
2019-06-12 23:26:16 -0700

    dictionary swiched to map

--- AnagramSolver/AnagramSolver.java
+++ AnagramSolver/AnagramSolver.java
@@ -1,57 +1,70 @@
 import java.util.*;
 
+import static java.util.stream.Collectors.toMap;
+
 public class AnagramSolver {
-    private List<String> dictionary;
+    private Map<String, LetterInventory> dictionary;
 
     public AnagramSolver(List<String> list) {
-        this.dictionary = list;
+//        this.dictionary = new HashMap<>();
+//        for (String word : list) {
+//            dictionary.put(word, new LetterInventory(word));
+//        }
+
+        this.dictionary = list.stream().collect(toMap(word -> word, LetterInventory::new));
     }
 
     public void print(String s, int max) {
-        print(new LetterInventory(s), max, new ArrayList(), s, reduceDictionary(s));
+        LetterInventory inventory = new LetterInventory(s);
+        print(inventory, max, new ArrayList<>(), inventory.size(), reduceDictionary(inventory));
     }
 
-    private void print(LetterInventory inventory, int max, List<String> anagram, String s, List<String> dictionary) {
-        if (max == 0 && inventory.isEmpty()) {
-            printAnagram(anagram, s);
+    private void print(LetterInventory current, int max, List<String> anagram, int inventorySize, Map<String, LetterInventory> dictionary) {
+        if (max == 0 && current.isEmpty()) {
+            printAnagram(anagram, inventorySize);
         }
-        else if (anagram.size() <= max && inventory.isEmpty()) {
-            printAnagram(anagram, s);
+        else if (anagram.size() <= max && current.isEmpty()) {
+            printAnagram(anagram, inventorySize);
         }
         else {
-            for (String word : dictionary) {
-                LetterInventory result = inventory.subtract(new LetterInventory(word));
+            for (Map.Entry<String, LetterInventory> entry : dictionary.entrySet()) {
+                LetterInventory result = current.subtract(entry.getValue());
                 if (result != null) {
-                    anagram.add(word);
-                    print(result, max, anagram, s, dictionary);
-                    anagram.remove(word);
+                    anagram.add(entry.getKey());
+                    print(result, max, anagram, inventorySize, dictionary);
+                    anagram.remove(entry.getKey());
                 }
             }
         }
     }
 
-    private List reduceDictionary(String s) {
-        List newDictionary = new ArrayList();
-        for (String element : this.dictionary) {
-            LetterInventory result = new LetterInventory(s).subtract(new LetterInventory(element));
-            if (result != null) {
-                newDictionary.add(element);
-            }
-        }
+    private Map<String, LetterInventory> reduceDictionary(LetterInventory inventory) {
+//        Map<String, LetterInventory> reducedDictionary = new HashMap<>();
+//        for (Map.Entry<String, LetterInventory> entry : this.dictionary.entrySet()) {
+//            LetterInventory result = inventory.subtract(entry.getValue());
+//            if (result != null) {
+//                reducedDictionary.put(entry.getKey(), entry.getValue());
+//            }
+//        }
+//
+//        return reducedDictionary;
 
-        return newDictionary;
+        return dictionary.entrySet().stream()
+                .filter(entry -> inventory.subtract(entry.getValue()) != null)
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
+
     }
 
-    private void printAnagram(List<String> anagram, String s) {
+    private void printAnagram(List<String> anagram, int inventorySize) {
 //        int counter = 0;
 //        for (String st : anagram) {
 //            counter += st.length();
-//            if (counter == new LetterInventory(s).size()) {
+//            if (counter == inventory.size()) {
 //                System.out.println(anagram);
 //            }
 //        }
 
-        if (new LetterInventory(s).size() == anagram.stream().mapToInt(String::length).sum()) {
+        if (inventorySize == anagram.stream().mapToInt(String::length).sum()) {
             System.out.println(anagram);
         }
     }
