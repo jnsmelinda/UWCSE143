74b86128a51ced178ec497b33c10357c4fee4512
jnsmelinda <jns.melinda@gmail.com>
2019-06-15 17:23:35 -0700

    (practice)Main added

---
+++ AnagramSolver/Main.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner input = new Scanner(new File("dict0.txt"));
+
+        List<String> dictionary = new ArrayList<String>();
+        while (input.hasNextLine()) {
+            dictionary.add(input.nextLine());
+        }
+
+	    AnagramSolver solver = new AnagramSolver(dictionary);
+        solver.print("georgebush", 0);
+    }
+}
